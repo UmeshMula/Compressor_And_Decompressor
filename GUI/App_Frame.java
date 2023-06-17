31f7a72d42e53a23a7a00fba770ce790ef462a6c
lenovo <umeshmula78@gmail.com>
2023-06-17 21:45:25 +0530

    first

--- GUI/App_Frame.java
+++ GUI/App_Frame.java
@@ -5,6 +5,7 @@ import comp_decomp.compressor;
 
 import javax.swing.*;
 import javax.swing.border.Border;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -15,20 +16,21 @@ public class App_Frame extends JFrame implements ActionListener {
     JButton CompressButton;
     JButton DecompressButton;
 
+    JLabel headingLabel;
+
     App_Frame() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(800, 500);
         this.getContentPane().setBackground(new Color(185, 158, 193)); // RGB(86, 125, 148) - AuroMetalSaurus
         this.setLayout(null);
+        this.setTitle("Compress-DeCompress");
 
         // Create and format the heading label
-        JLabel headingLabel = new JLabel("<html><u style='text-decoration: underline; text-decoration-style: Dotted ; text-decoration-color: rgb(79, 33, 100); font-weight: bold;'>Compress & DeCompress</u></html>");
-        headingLabel.setBounds(0, 20, this.getWidth(), 100);
+        headingLabel = new JLabel("<html><u style='text-decoration: underline; text-decoration-style: Dotted ; text-decoration-color: rgb(79, 33, 100); font-weight: bold; font-size: 40px;'>Compress & DeCompress</u></html>");
+        headingLabel.setBounds(0, 0, this.getWidth(), 100);
         headingLabel.setHorizontalAlignment(SwingConstants.CENTER);
-        Font headingFont = new Font("Footlight MT Light", Font.PLAIN | Font.ITALIC , 40);
-        headingLabel.setFont(headingFont);
+        headingLabel.setFont(new Font("Footlight MT Light", Font.PLAIN | Font.ITALIC, 40));
         headingLabel.setForeground(new Color(75, 28, 113)); // RGB(79, 33, 100) - Custom Color
-
 
         CompressButton = new JButton("Select File To Compress");
         CompressButton.setBounds(200, 160, 375, 40);
@@ -56,10 +58,8 @@ public class App_Frame extends JFrame implements ActionListener {
         this.add(CompressButton);
         this.add(DecompressButton);
 
-        Font buttonFont1 = new Font("Monotype Corsiva", Font.PLAIN |  Font.ITALIC, 25);  // Font for CompressButton
-        Font buttonFont2 = new Font("Script MT Bold", Font.ITALIC, 25);  // Font for DecompressButton
-        CompressButton.setFont(buttonFont1);
-        DecompressButton.setFont(buttonFont2);
+        CompressButton.setFont(new Font("Monotype Corsiva", Font.PLAIN | Font.ITALIC, 25));  // Font for CompressButton
+        DecompressButton.setFont(new Font("Script MT Bold", Font.ITALIC, 25));  // Font for DecompressButton
 
         this.setVisible(true);
     }
@@ -75,28 +75,79 @@ public class App_Frame extends JFrame implements ActionListener {
         return image;
     }
 
+    private String showFileTypeSelectionDialog() {
+        Object[] options = {"PNG", "ZIP", "DOC", "WORD", "PDF"};
+        int selection = JOptionPane.showOptionDialog(this, "Select file type to compress:", "File Type Selection", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
+        if (selection == -1) {
+            return null;
+        }
+        return options[selection].toString().toLowerCase();
+    }
+
+    private int showQualitySelectionDialog() {
+        String input = JOptionPane.showInputDialog(this, "Enter quality range (1-10):");
+        int quality = -1;
+        try {
+            quality = Integer.parseInt(input);
+            int percentage = quality * 10; // Calculate the percentage
+            JOptionPane.showMessageDialog(this, "Compression level selected: " + percentage + "%");
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(this, "Invalid input. Quality range should be an integer between 1 and 10.");
+        }
+        return quality;
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == CompressButton) {
-            JFileChooser filechooser = new JFileChooser();
-            int response = filechooser.showSaveDialog(null);
-            if (response == JFileChooser.APPROVE_OPTION) {
-                File file = new File(filechooser.getSelectedFile().getAbsolutePath());
-                System.out.print(file);
+            String fileType = showFileTypeSelectionDialog();
+            if (fileType != null) {
+                JFileChooser fileChooser = new JFileChooser();
 
-                try {
-                    compressor.method(file);
-                } catch (Exception ee) {
-                    JOptionPane.showMessageDialog(null, ee.toString());
+                // Add file filter
+                FileNameExtensionFilter fileFilter = new FileNameExtensionFilter(fileType.toUpperCase() + " Files", fileType);
+                fileChooser.addChoosableFileFilter(fileFilter);
+                fileChooser.setFileFilter(fileFilter); // Set the default filter
+
+                int response = fileChooser.showSaveDialog(null);
+                if (response == JFileChooser.APPROVE_OPTION) {
+                    File file = fileChooser.getSelectedFile();
+                    System.out.print(file);
+
+                    int quality = showQualitySelectionDialog();
+                    if (quality >= 1 && quality <= 10) {
+                        try {
+                            compressor.compressFile(file, quality);
+                        } catch (Exception ee) {
+                            JOptionPane.showMessageDialog(null, ee.toString());
+                        }
+                    }
                 }
             }
         }
         if (e.getSource() == DecompressButton) {
-            JFileChooser filechooser = new JFileChooser();
-            int response = filechooser.showSaveDialog(null);
+            JFileChooser fileChooser = new JFileChooser();
+
+            // Add file filters
+            FileNameExtensionFilter pngFilter = new FileNameExtensionFilter("PNG Files", "png");
+            FileNameExtensionFilter zipFilter = new FileNameExtensionFilter("ZIP Files", "zip");
+            FileNameExtensionFilter docFilter = new FileNameExtensionFilter("DOC Files", "doc");
+            FileNameExtensionFilter wordFilter = new FileNameExtensionFilter("WORD Files", "word");
+            FileNameExtensionFilter pdfFilter = new FileNameExtensionFilter("PDF Files", "pdf");
+
+            // Set the file filters
+            fileChooser.addChoosableFileFilter(pngFilter);
+            fileChooser.addChoosableFileFilter(zipFilter);
+            fileChooser.addChoosableFileFilter(docFilter);
+            fileChooser.addChoosableFileFilter(wordFilter);
+            fileChooser.addChoosableFileFilter(pdfFilter);
+            fileChooser.setFileFilter(pngFilter); // Set the default filter
+
+            int response = fileChooser.showOpenDialog(null);
             if (response == JFileChooser.APPROVE_OPTION) {
-                File file = new File(filechooser.getSelectedFile().getAbsolutePath());
+                File file = fileChooser.getSelectedFile();
                 System.out.print(file);
+
                 try {
                     Decompressor.method(file);
                 } catch (Exception ee) {
@@ -107,7 +158,10 @@ public class App_Frame extends JFrame implements ActionListener {
     }
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new App_Frame());
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new App_Frame();
+            }
+        });
     }
 }
-
--- comp_decomp/compressor.java
+++ comp_decomp/compressor.java
@@ -1,34 +1,89 @@
 package comp_decomp;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.io.*;
+import java.util.zip.Deflater;
 import java.util.zip.GZIPOutputStream;
 
 public class compressor {
-    public static void method(File file) throws IOException
-    {
+    private static final int[] COMPRESSION_PERCENTAGES = {
+            10, 20, 30, 40, 50, 60, 70, 80, 90, 100
+    };
+
+    public static void compressFile(File file, int compressionPercentage) throws IOException {
         String fileDirectory = file.getParent();
         FileInputStream fis = new FileInputStream(file);
-        FileOutputStream fos = new FileOutputStream(fileDirectory+"/CompressedFile.gz");
-        GZIPOutputStream gzip = new GZIPOutputStream(fos);
+        FileOutputStream fos = new FileOutputStream(fileDirectory + "/CompressedFile.gz");
+
+        int compressionLevel = getCompressionLevel(compressionPercentage);
+        Deflater deflater = new Deflater(compressionLevel);
+        GZIPOutputStream gzip = new GZIPOutputStream(fos) {
+            {
+                def.setLevel(compressionLevel);
+            }
+        };
 
         byte[] buffer = new byte[1024];
         int len;
-        while((len=fis.read(buffer)) != -1)
-        {
-            gzip.write (buffer,0,len);
+        while ((len = fis.read(buffer)) != -1) {
+            gzip.write(buffer, 0, len);
         }
+
         gzip.close();
         fos.close();
         fis.close();
     }
-    public static void main(String[] args) throws IOException
-    {
-        File path = new File("/Users/UmeshAc/Downloads/Compressor/CompressorDecompressor/Testfile");
-        method(path);
 
+    private static int getCompressionLevel(int compressionPercentage) {
+        int index = (compressionPercentage - 1) / 10;  // Mapping percentage to array index
+        index = Math.max(0, Math.min(index, COMPRESSION_PERCENTAGES.length - 1));  // Clamp index within array bounds
+        return index;
     }
 
+    public static void main(String[] args) throws IOException {
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        fileChooser.setAcceptAllFileFilterUsed(false);
+
+        // File type filter selection
+        FileNameExtensionFilter filter = null;
+        String fileTypeSelection = JOptionPane.showInputDialog("Select the file type to compress (jpg, pdf, word, zip, document):");
+        switch (fileTypeSelection.toLowerCase()) {
+            case "jpg":
+                filter = new FileNameExtensionFilter("JPEG Images", "jpg", "jpeg");
+                break;
+            case "pdf":
+                filter = new FileNameExtensionFilter("PDF Documents", "pdf");
+                break;
+            case "word":
+                filter = new FileNameExtensionFilter("Microsoft Word Documents", "doc", "docx");
+                break;
+            case "zip":
+                filter = new FileNameExtensionFilter("ZIP Archives", "zip");
+                break;
+            case "png":
+                filter = new FileNameExtensionFilter("PNG Images", "png");
+                break;
+            case "document":
+                filter = new FileNameExtensionFilter("All Documents", "jpg", "jpeg", "pdf", "doc", "docx", "zip", "png");
+                break;
+            default:
+                JOptionPane.showMessageDialog(null, "Invalid file type selection!");
+                System.exit(1);
+        }
+
+        fileChooser.addChoosableFileFilter(filter);
+        fileChooser.setFileFilter(filter); // Set the selected file filter as the default filter
+
+        int result = fileChooser.showOpenDialog(null);
+        if (result == JFileChooser.APPROVE_OPTION) {
+            File selectedFile = fileChooser.getSelectedFile();
+
+            String compressionPercentageInput = JOptionPane.showInputDialog("Select the compression percentage (10-100):");
+            int compressionPercentage = Integer.parseInt(compressionPercentageInput);
+
+            compressFile(selectedFile, compressionPercentage);
+        }
+    }
 }
